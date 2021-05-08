77fc7f35f9b4f8ab3e8d96313edf8c32f415fdf3
Stephane Baroux <stephane.baroux@gmail.com>
2021-05-08 16:49:44 +0200

    working modal with dimming background

--- src/Containers/Modal.js
+++ src/Containers/Modal.js
@@ -12,31 +12,33 @@ const Modal = (props) => {
     const handleToUpdate = props.handleToUpdate;
 
     return(
-        <div className="card modalContainer" id="modal">
-            <div className="modalHeader">
-                <h1>Back this project</h1>
-                <img src={iconCloseModal} alt="close modal button" onClick={handleToUpdate}/>
-            </div>
-            <p>Want to support us in bringing Mastercraft Bamboo Monitor Rise out in the world?</p>
-            <PledgeSection
-                title = "Bamboo Stand"
-                subtitle= "Pledge $25 or more"
-                text = "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list."
-                value = "101"
-            />
-            <PledgeSection
-                title = "Black Edition Stand"
-                subtitle= "Pledge $75 or more"
-                text = "You get a Black Special Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included."
-                value = "64"
-            />
-            <PledgeSection
-                title = "Mahogany Special Edition"
-                subtitle= "Pledge $200 or more"
-                text = "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You'll be added to our Backer member list. Shipping is included."
-                value = "0"
-            />
+        <div className="modalContainer" id="modal">
             <hr className="modalBackground" />
+            <div className="card">
+                <div className="modalHeader">
+                    <h1>Back this project</h1>
+                    <img src={iconCloseModal} alt="close modal button" onClick={handleToUpdate}/>
+                </div>
+                <p>Want to support us in bringing Mastercraft Bamboo Monitor Rise out in the world?</p>
+                <PledgeSection
+                    title = "Bamboo Stand"
+                    subtitle= "Pledge $25 or more"
+                    text = "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list."
+                    value = "101"
+                />
+                <PledgeSection
+                    title = "Black Edition Stand"
+                    subtitle= "Pledge $75 or more"
+                    text = "You get a Black Special Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included."
+                    value = "64"
+                />
+                <PledgeSection
+                    title = "Mahogany Special Edition"
+                    subtitle= "Pledge $200 or more"
+                    text = "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You'll be added to our Backer member list. Shipping is included."
+                    value = "0"
+                />
+            </div>
         </div>
     );
 }
