1afdedde2305755670c989a37c8092b4f3bd0112
Stephane Baroux <stephane.baroux@gmail.com>
2021-05-14 17:18:59 +0200

    removed id on input boxes as they get duplicated

--- src/Components/PledgeExtension.js
+++ src/Components/PledgeExtension.js
@@ -8,7 +8,7 @@ const PledgeExtension = (props) =>{
             <p>Enter your pledge</p>
             <div className="inputs">
                 <span className="pledgeInputBox">
-                    $ <label htmlFor="pledgeInput"><input type="text" name="pledgeInput" id="pledgeInput" autoFocus /></label>
+                    $ <label htmlFor="pledgeInput"><input type="text" name="pledgeInput" autoFocus /></label>
                 </span>
                 <Button className="btnPledge"
                     display ="inline"
